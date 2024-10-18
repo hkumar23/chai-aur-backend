07563f8dd57ffa655ada77d1f8c1dd90d6799c88
Harsh Kumar <harshkumar_se21a14_23@dtu.ac.in>
2024-10-19 02:24:02 +0530

    custom api response and user handling

---
+++ src/utils/ApiError.js
@@ -0,0 +1,25 @@
+// Error class is provided by node
+class ApiError extends Error{
+    constructor(
+        statusCode,
+        message="Something went wrong",
+        error=[],
+        stack="",
+    ){
+        super(message)
+        this.statusCode=statusCode
+        this.data=null
+        this.message=message
+        this.success=false
+        this.errors=this.errors
+
+        if(stack){
+            this.stack=stack
+        }
+        else{
+            Error.captureStackTrace(this,this.constructor)
+        }
+    }
+}
+
+export {ApiError}
