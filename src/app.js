4937c268294b99432a6d7baf8bebffa7392482f3
Harsh Kumar <harshkumar_se21a14_23@dtu.ac.in>
2024-10-26 02:53:49 +0530

    add user controllers and routes

--- src/app.js
+++ src/app.js
@@ -9,9 +9,18 @@ app.use(cors({
     credentials: true
 }))
 
+// Middleware functions are applied using app.use()
 app.use(express.json({limit:"16kb"}))
-app.use(express.urlencoded({extended:true,limit:"16kb"}))
+app.use(express.urlencoded({extended:true,limit:"16kb"})) // when request is from some url
 app.use(express.static("public"))
-app.use(cookieParser())
+app.use(cookieParser()) // Parses cookies attached to the client’s request
 
+
+// routes import
+import userRouter from "../src/routes/user.routes.js";
+
+// routes declaration
+app.use("/api/v1/users",userRouter)
+
+// https://localhost:8000/api/v1/users/register
 export {app}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 // require('dotenv').config({path: './env'})
 import dotenv from "dotenv"; //this is consistent
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
 //dotenv make all evironment var available
 dotenv.config({
     path:'./env'
