3f17730d6278c6d752b0ea2f05071f5518d67dd7
Harsh Kumar <harshkumar_se21a14_23@dtu.ac.in>
2024-10-28 20:22:45 +0530

    add register user controller

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -54,7 +54,7 @@ const userSchema=new Schema(
 userSchema.pre("save",async function (next) {
     if(!this.isModified("password"))return next();
 
-    this.password=bcrypt.hash(this.password,10);
+    this.password=await bcrypt.hash(this.password,10);
     next();
 })
 
@@ -66,17 +66,6 @@ userSchema.methods.generateAccessToken=function(){ //this process don't take tha
     jwt.sign(
         {
             _id: this._id,
-        },
-        process.env.REFRESH_TOKEN_SECRET,
-        {
-            expiresIn:process.env.REFRESH_TOKEN_EXPIRY
-        }
-    );
-}
-userSchema.methods.generateRefreshToken=function(){ //this process don't take that much time we're not making it async
-    jwt.sign(
-        {
-            _id: this._id,
             email: this.email,
             username: this.username,
             fullName: this.fullName,
@@ -84,6 +73,17 @@ userSchema.methods.generateRefreshToken=function(){ //this process don't take th
         process.env.ACCESS_TOKEN_SECRET,
         {
             expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+        }
+    );
+}
+userSchema.methods.generateRefreshToken=function(){ //this process don't take that much time we're not making it async
+    jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn:process.env.REFRESH_TOKEN_EXPIRY
         }
     );
 }
